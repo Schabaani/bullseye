d8d52524b38025a1f00f8f9e828a73012700a96b
Amish <amir.shabani68@gmail.com>
2023-02-18 15:24:39 +0330

    init commit

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1280;
+  canvas.height = 720;
+});
